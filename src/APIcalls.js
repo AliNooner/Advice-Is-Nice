8ec9aa46eb9c831b0593fff1ec329239c324ded3
Alison Nooner <alinooner@gmail.com>
2022-04-20 17:18:19 -0500

    Finish setting up fetch calls

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -12,18 +12,23 @@
 //   }
 // }
 
-const fetchCalls = {
-  fetchData: function (endpoint) {
-    return fetch(`https://api.adviceslip.com/advice/${endpoint}`)
-      .then(response => {
-        if (response.status === 500){
-          throw new Error('500 - Server Not Responding.')
-        } else if (response.status === 404){
-          throw new Error('404 - URL Not Found.')
-        }
-      return response.json()})
-      .catch(error => window.alert(`${error} Refresh browser to try again.` ))
-  }
+// const fetchCalls = {
+//   fetchData: function (endpoint) {
+//     return fetch(`https://api.adviceslip.com/advice/${endpoint}`)
+//       .then(response => {
+//         if (response.status === 500){
+//           throw new Error('500 - Server Not Responding.')
+//         } else if (response.status === 404){
+//           throw new Error('404 - URL Not Found.')
+//         }
+//       return response.json()})
+//       .catch(error => window.alert(`${error} Refresh browser to try again.` ))
+//   }
+// }
+
+const allAdviceData = () => {
+  return fetch('https://api.adviceslip.com/advice')
+  .then(response => response.json())
 }
 
-export default fetchCalls;
+export default allAdviceData;
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import allAdviceData from '../../APIcalls';
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      allAdvice: [],
+      isFavorited: false,
+      pieceOfAdvice: {}
     }
   }
 
+  // componentDidMount = () => {
+  //   fetchCalls
+  //     .fetchData('allAdvice')
+  //     .then((data) => {
+  //       const newArray = data.map((advice) => {
+  //         advice.isFavorited = false
+  //         advice.key = advice.id
+  //         return advice
+  //       })
+  //       this.setState({allAdvice: newArray})
+  //     })
+  //     .then(() => this.getRandomAdvice(this.state.allAdvice))
+  // }
+
+  // getRandomAdvice = (array) => {
+  //   const index = Math.floor(Math.random() * array.length)
+  //   this.setState({pieceOfAdvice: this.state.allAdvice[index]})
+  // }
+
+  componentDidMount = () => {
+  return allAdviceData()
+    .then((data) => this.setState({ allAdvice: data.slip}))
+    .catch((error) =>
+      this.setState({ hasError: true, error: `Oops! Something went wrong!` })
+    );
+};
+
   render() {
     return (
-      <div>Hey!</div>
+      <main className='App'>
+        <div>Hi!</div>
+      </main>
     )
   }
 }
