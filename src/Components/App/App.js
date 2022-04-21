acf3619abc63f454e43d6e26f2646ff1f7e478b1
Alison Nooner <alinooner@gmail.com>
2022-04-21 08:36:41 -0500

    Add Daily Advice component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import allAdviceData from '../../APIcalls';
-import Nav from '../Nav/Nav'
+import Nav from '../Nav/Nav';
+import DailyAdvice from '../DailyAdvice/DailyAdvice';
 
 
 class App extends Component {
@@ -20,25 +21,62 @@ class App extends Component {
   //   this.setState({pieceOfAdvice: this.state.allAdvice[index]})
   // }
 
-  componentDidMount = () => {
+componentDidMount = () => {
   return allAdviceData()
-    .then((data) => this.setState({ allAdvice: data.slip}))
+    .then((data) => this.setState({ allAdvice: data.slip }))
     .catch((error) =>
       this.setState({ hasError: true, error: `Oops! Something went wrong!` })
     );
 };
 
-  render() {
-    return (
-      <main className='App'>
-				<Route
-					exact
-					path='/'
-					render={() => <Nav />}
-				/>
-      </main>
-    )
-  }
+
+render() {
+return (
+  <main className='App'>
+    <Nav />
+    <Route
+      exact
+      path='/'
+      render={() => <DailyAdvice />}
+    />
+    <Switch>
+      <Route
+        exact
+        path='/'
+        render={() => (
+          <div>
+            <p className='favorite-message-tag'>
+              Click the heart icon to add message to your favorites
+            </p>
+            <AllAdvice
+              // allBirds={this.state.allBirds}
+              // changeIcon={this.changeIcon}
+            />
+          </div>
+        )}
+      />
+      <Route
+        exact
+        path='/favorites'
+        render={() => (
+          <Favorites
+            // lifers={this.state.lifers}
+            // allBirds={this.state.allBirds}
+            // changeIcon={this.changeIcon}
+          />
+        )}
+      />
+    <Route
+    render={() => (
+      <div className='no-match'>404 Error: Sorry, the URL/page you requested was not found.</div>)}
+      />
+      </Switch>
+    <footer>
+      <img src={logo} alt='Fowl Prowl Logo' id='logo' />
+    </footer>
+  </main>
+)
+}
 }
 
 export default App;
---
+++ src/Components/DailyAdvice/DailyAdvice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './DailyAdvice.css';
+import { Route } from 'react-router-dom';
+
+const DailyAdvice = () => {
+
+  return (
+    <div className='random-advice'>Daily Advice goes here</div>
+  )
+
+}
+
+export default DailyAdvice;
