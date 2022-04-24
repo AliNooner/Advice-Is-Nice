c00488719218bdfeb4d2c1b5ff321fd6a80ecb47
Alison Nooner <alinooner@gmail.com>
2022-04-23 20:37:10 -0500

    Refactor

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,10 +1,7 @@
-describe ('Feedback loop user flows', () => {
-  it('Should confirm true is equal to true', () => {
-    expect(true).to.equal(true)
-  })
+describe ('Main page user flow', () => {
 
   it('Should have a header with Just a Thought on page load', () => {
-    cy.visit('http://localhost:3002/').contains('h1', 'Just a Thought')
+    cy.visit('http://localhost:3000/').contains('h1', 'Just a Thought')
   })
 
   // test buttons are on page load
@@ -26,24 +23,14 @@ describe ('Feedback loop user flows', () => {
     cy.get('.favorites-link').should('have.text', 'Favorites').click()
   })
 
-
   it('Should take the user to the Favorites page on button click', () => {
     cy.get('.favorites-link').click()
-    cy.url().should('eq', 'http://localhost:3002/favorites')
+    cy.url().should('eq', 'http://localhost:3000/favorites')
   })
-
-  //add fetch testing on button click
-  //use stubbing
-  //create fixture file
-
-  // it('Should display a piece of advice on button click', () => {
-  //   cy.get('.advice-button').click()
-  //   cy.includes('.single-advice')
-  // })
 
   it('Should display a piece of advice on button click', () => {
     cy.intercept('https://api.adviceslip.com/advice', {fixture: 'advice-info.json'})
-    .visit('http://localhost:3002/')
+    .visit('http://localhost:3000/')
     cy.get('.advice-button').should('be.visible').click()
     cy.wait(2000)
     cy.get('h2').contains('Hold the door open for the next person.')
